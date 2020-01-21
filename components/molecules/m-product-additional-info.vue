954077b3bda7ed54779e4e4bc8ec3222738ad4ae
tkostuch <tkostuch@divante.pl>
2020-01-21 16:41:07 +0100

    Feature: 50 - updated smoothscroll - added review list on product cart - added modal for adding review - update sfui - added product-attributes

--- components/molecules/m-product-additional-info.vue
+++ components/molecules/m-product-additional-info.vue
@@ -1,97 +1,81 @@
 <template>
-  <SfTabs class="m-product-additional-info" :open-tab="2">
-    <SfTab title="Description">
-      <div>
-        <p>
-          The Karissa V-Neck Tee features a semi-fitted shape that's flattering
-          for every figure. You can hit the gym with confidence while it hugs
-          curves and hides common "problem" areas. Find stunning women's
-          cocktail dresses and party dresses.
-        </p>
-      </div>
+  <SfTabs class="m-product-additional-info" :open-tab="openTab" id="m-product-additional-info">
+    <SfTab :title="$t('Description')">
+      <div itemprop="description" v-html="product.description" />
       <div class="m-product-additional-info__properties">
-        <SfProperty
-          v-for="(property, i) in properties"
+        <AProductAttribute
+          v-for="(attribute, i) in attributes"
           :key="i"
-          class="m-product-additional-info__property"
-          :name="property.name"
-          :value="property.value"
+          :product="product"
+          :attribute="attribute"
         />
       </div>
     </SfTab>
-    <SfTab title="Read reviews">
-      <SfReview
-        v-for="(review, i) in reviews"
-        :key="i"
-        class="m-product-additional-info__review"
-        :author="review.author"
-        :date="review.date"
-        :message="review.message"
-        :rating="review.rating"
-        :max-rating="5"
-      />
-    </SfTab>
-    <SfTab title="Additional Information">
-      <SfHeading
-        title="Brand"
-        :level="3"
-        class="sf-heading--no-underline sf-heading--left"
-      />
-      <p>
-        <u>Brand name</u> is the perfect pairing of quality and design. This
-        label creates major everyday vibes with its collection of modern
-        brooches, silver and gold jewellery, or clips it back with hair
-        accessories in geo styles.
-      </p>
+    <SfTab :title="$t('Read reviews')">
+      <div class="m-product-additional-info__review-header">
+        <SfHeading
+          :title="$t('{count} Reviews', { count: reviewsCount })"
+          :level="3"
+          class="sf-heading--left"
+        />
+        <AProductRating
+          @click:stars="handleOpenReviewModal"
+          @click:text="handleOpenReviewModal"
+          :reviews="reviews"
+          :text="$t('Leave me review')"
+        />
+      </div>
+      <SfDivider v-show="reviewsCount"/>
+      <MReviewList v-show="reviewsCount" :reviews="reviews" />
     </SfTab>
   </SfTabs>
 </template>
 <script>
-import { SfProperty, SfHeading, SfTabs, SfReview } from '@storefront-ui/vue';
+import { mapMutations } from 'vuex'
+import { SfHeading, SfTabs, SfDivider } from '@storefront-ui/vue';
+import AProductRating from 'theme/components/atoms/a-product-rating';
+import AProductAttribute from 'theme/components/atoms/a-product-attribute';
+import MReviewList from 'theme/components/molecules/m-review-list';
 export default {
   components: {
-    SfProperty,
     SfHeading,
     SfTabs,
-    SfReview
+    AProductRating,
+    SfDivider,
+    AProductAttribute,
+    MReviewList
+  },
+  props: {
+    reviews: {
+      type: Array,
+      default: () => []
+    },
+    attributes: {
+      type: Array,
+      default: () => []
+    },
+    product: {
+      type: Object,
+      default: () => ({})
+    }
   },
   data () {
     return {
-      properties: [
-        {
-          name: 'Product Code',
-          value: '578902-00'
-        },
-        {
-          name: 'Category',
-          value: 'Pants'
-        },
-        {
-          name: 'Material',
-          value: 'Cotton'
-        },
-        {
-          name: 'Country',
-          value: 'Germany'
-        }
-      ],
-      reviews: [
-        {
-          author: 'Jane D.Smith',
-          date: 'April 2019',
-          message:
-            "I was looking for a bright light for the kitchen but wanted some item more modern than a strip light. this one is perfect, very bright and looks great. I can't comment on interlation as I had an electrition instal it. Would recommend",
-          rating: 4
-        },
-        {
-          author: 'Mari',
-          date: 'Jan 2018',
-          message:
-            'Excellent light output from this led fitting. Relatively easy to fix to the ceiling,but having two people makes it easier, to complete the installation. Unable to comment on reliability at this time, but I am hopeful of years of use with good light levels. Excellent light output from this led fitting. Relatively easy to fix to the ceiling,',
-          rating: 5
-        }
-      ]
+      openTab: 2
     };
+  },
+  computed: {
+    reviewsCount () {
+      return this.reviews.length
+    }
+  },
+  methods: {
+    ...mapMutations('ui', {
+      openModal: 'openModal'
+    }),
+    handleOpenReviewModal () {
+      this.openModal('product-review')
+    }
   }
 };
 </script>
@@ -115,21 +99,22 @@ export default {
   &__properties {
     margin-top: $spacer-big;
   }
-  &__property {
-    padding: $spacer-small 0;
-  }
-  &__review {
-    padding-bottom: $spacer-big;
-    @include for-desktop {
-      padding-bottom: $spacer-extra-big;
-      border-bottom: 1px solid $c-light;
-    }
-    & + & {
-      padding-top: $spacer-extra-big;
-      border-top: 1px solid $c-light;
-      @include for-desktop {
-        border-top: 0;
-        padding-top: $spacer-extra-big;
+  &__review-header {
+    display: flex;
+    ::v-deep {
+      .sf-heading {
+        flex-shrink: 0;
+      }
+      .a-product-rating {
+        margin-left: 10px;
+        width: 100%;
+        justify-content: space-between;
+      }
+      .sf-rating {
+        .sf-icon {
+          width: 20px;
+          height: 20px;
+        }
       }
     }
   }
