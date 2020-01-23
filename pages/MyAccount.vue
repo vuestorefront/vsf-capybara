9dc1ae04c8626b502abc7519d4844bebde8806b7
Tomasz Kikowski <tomaszkikowski@gmail.com>
2020-01-23 10:46:25 +0100

    #60 I want to open My Account

--- pages/MyAccount.vue
+++ pages/MyAccount.vue
@@ -1,134 +1,118 @@
 <template>
-  <div id="my_account">
-    <div class="bg-cl-secondary py35 pl20">
-      <div class="container">
-        <breadcrumbs :with-homepage="true" active-route="My Account" />
-        <h1>
-          {{ $t("My Account") }}
-        </h1>
-      </div>
-    </div>
-
-    <div class="container pt45 pb70">
-      <div class="row px20 pt0">
-        <div class="col-md-3 hidden-xs hidden-sm block">
-          <nav class="static-menu serif h4 mb35">
-            <ul class="m0 p0">
-              <li
-                v-for="(page, index) in navigation"
-                :key="index"
-                class="mb20"
-                @click="notify(page.title)"
-              >
-                <router-link :to="localizedRoute(page.link)" class="cl-accent">
-                  {{ page.title }}
-                </router-link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div class="col-md-9">
-          <no-ssr>
-            <component :is="this.$props.activeBlock" />
-          </no-ssr>
-        </div>
-      </div>
-    </div>
+  <div id="my-account">
+    <SfBreadcrumbs class="breadcrumbs desktop-only" :breadcrumbs="breadcrumbs">
+      <template #link="{breadcrumb}">
+        <router-link :to="breadcrumb.route.link">
+          {{ breadcrumb.text }}
+        </router-link>
+      </template>
+    </SfBreadcrumbs>
+    <SfContentPages
+      :title="$t('My Account')"
+      :active="activePage"
+      class="my-account"
+      @click:change="changeActivePage"
+    >
+      <SfContentCategory :title="$t('Personal Details')">
+        <SfContentPage :title="$t('My profile')">
+          <MyProfile />
+        </SfContentPage>
+        <SfContentPage :title="$t('Shipping details')">
+          <MyShippingDetails />
+        </SfContentPage>
+        <SfContentPage :title="$t('Loyalty Card')" />
+        <SfContentPage :title="$t('My newsletter')">
+          <MyNewsletter />
+        </SfContentPage>
+      </SfContentCategory>
+      <SfContentCategory :title="$t('Order details')">
+        <SfContentPage :title="$t('Order history')">
+          <MyOrders />
+        </SfContentPage>
+        <SfContentPage :title="$t('My reviews')" />
+      </SfContentCategory>
+      <SfContentPage :title="$t('Log out')" />
+    </SfContentPages>
   </div>
 </template>
 
 <script>
 import MyAccount from '@vue-storefront/core/pages/MyAccount';
-import Breadcrumbs from '../components/core/Breadcrumbs';
 import MyProfile from '../components/core/blocks/MyAccount/MyProfile';
 import MyShippingDetails from '../components/core/blocks/MyAccount/MyShippingDetails';
 import MyNewsletter from '../components/core/blocks/MyAccount/MyNewsletter';
 import MyOrders from '../components/core/blocks/MyAccount/MyOrders';
-import MyOrder from '../components/core/blocks/MyAccount/MyOrder';
-import MyRecentlyViewed from '../components/core/blocks/MyAccount/MyRecentlyViewed';
-import NoSSR from 'vue-no-ssr';
-import { RecentlyViewedModule } from '@vue-storefront/core/modules/recently-viewed';
-import { registerModule } from '@vue-storefront/core/lib/modules';
+
+import { localizedRoute } from '@vue-storefront/core/lib/multistore';
+import { SfBreadcrumbs, SfContentPages } from '@storefront-ui/vue';
 
 export default {
   components: {
-    Breadcrumbs,
+    SfBreadcrumbs,
+    SfContentPages,
     MyProfile,
     MyShippingDetails,
     MyNewsletter,
-    MyOrders,
-    MyOrder,
-    MyRecentlyViewed,
-    'no-ssr': NoSSR
+    MyOrders
   },
   mixins: [MyAccount],
   data () {
     return {
-      navigation: [
-        { title: this.$t('My profile'), link: '/my-account' },
+      activePage: this.$t('My profile'),
+      breadcrumbs: [
         {
-          title: this.$t('My shipping details'),
-          link: '/my-account/shipping-details'
+          'text': this.$t('Home'),
+          'route': {
+            'link': localizedRoute('/')
+          }
         },
-        { title: this.$t('My newsletter'), link: '/my-account/newsletter' },
-        { title: this.$t('My orders'), link: '/my-account/orders' },
-        { title: this.$t('My loyalty card'), link: '#' },
-        { title: this.$t('My product reviews'), link: '#' },
         {
-          title: this.$t('My Recently viewed products'),
-          link: '/my-account/recently-viewed'
+          'text': this.$t('My account'),
+          'route': {
+            'link': localizedRoute('/my-account')
+          }
         }
       ]
     };
   },
-  beforeCreate () {
-    registerModule(RecentlyViewedModule);
-  },
   methods: {
-    notify (title) {
-      if (title === 'My loyalty card' || title === 'My product reviews') {
-        this.$store.dispatch('notification/spawnNotification', {
-          type: 'warning',
-          message: this.$t(
-            'This feature is not implemented yet! Please take a look at https://github.com/DivanteLtd/vue-storefront/issues for our Roadmap!'
-          ),
-          action1: { label: this.$t('OK') }
-        });
+    changeActivePage(title) {
+      if (title === "Log out") {
+        this.logout();
+        return;
       }
+      this.activePage = title;
+    },
+    async logout () {
+      await this.$store.dispatch('user/logout', {});
+      this.$router.push(this.localizedRoute('/'));
     }
   }
 };
 </script>
 
-<style lang="scss">
-@import "~theme/css/base/text";
-@import "~theme/css/variables/colors";
-@import "~theme/css/helpers/functions/color";
-$color-tertiary: color(tertiary);
+<style lang="scss" scoped>
+@import "~@storefront-ui/shared/styles/_variables.scss";
 
-.static-menu {
-  ul {
-    list-style: none;
+@mixin for-desktop {
+  @media screen and (min-width: $desktop-min) {
+    @content;
   }
+}
 
-  a {
-    &:after {
-      content: "";
-      display: block;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 1px;
-      background-color: $color-tertiary;
-    }
-
-    &:hover,
-    &.router-link-exact-active {
-      &:after {
-        opacity: 0;
-      }
-    }
+#my-account {
+  box-sizing: border-box;
+  @include for-desktop {
+    max-width: 1240px;
+    margin: auto;
   }
+}
+
+.breadcrumbs {
+  padding: $spacer-big $spacer-extra-big $spacer-extra-big;
+}
+
+.main {
+  display: flex;
 }
 </style>
