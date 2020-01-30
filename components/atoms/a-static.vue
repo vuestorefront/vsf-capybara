63e2e702e5d087f11d0f212f4bb3e764ed9988de
Paweł Smyrek <psmyrek@divante.pl>
2020-01-30 12:47:33 +0100

    Static page from Storefront UI

---
+++ components/atoms/a-static.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="a-static">
+    <SfTabs v-if="showTabs" :open-tab="1">
+      <SfTab v-for="c in content" :key="c.title" :title="c.title">
+        <div v-html="$sanitize(c.message)" />
+      </SfTab>
+    </SfTabs>
+    <div v-else>
+      <SfHeading :level="1" :title="content.title" />
+      <div v-html="$sanitize(content.message)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { SfTabs, SfHeading } from '@storefront-ui/vue';
+
+export default {
+  name: 'AStatic',
+  components: {
+    SfTabs,
+    SfHeading
+  },
+  props: {
+    content: {
+      type: [Object, Array],
+      default: () => ({
+        title: '',
+        message: ''
+      })
+    }
+  },
+  computed: {
+    showTabs () {
+      return Array.isArray(this.content);
+    }
+  }
+};
+</script>
