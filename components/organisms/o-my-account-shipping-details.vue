c71530f1557b383b6fb18bad5ac258ee278e9e06
Tomasz Kikowski <tomaszkikowski@gmail.com>
2020-02-04 13:25:10 +0100

    #61 My account addresses

--- components/organisms/o-my-account-shipping-details.vue
+++ components/organisms/o-my-account-shipping-details.vue
@@ -9,21 +9,29 @@
       >
         <SfTab :title="$t('Change the address')">
           <p class="message">
-            {{ $T('Keep your addresses and contact details updated.') }}
+            {{ $t('Keep your addresses and contact details updated.') }}
           </p>
           <div class="form">
             <SfInput
-              v-model="editedAddress.firstName"
-              name="firstName"
+              v-model="editedAddress.firstname"
+              name="firstname"
               label="First Name"
               required
+              :valid="!$v.editedAddress.firstname.$error"
+              :error-message="
+                !$v.editedAddress.firstname.required
+                  ? $t('Field is required.')
+                  : $t('Name must have at least 2 letters.')
+              "
               class="form__element form__element--half"
             />
             <SfInput
-              v-model="editedAddress.lastName"
-              name="lastName"
+              v-model="editedAddress.lastname"
+              name="lastname"
               label="Last Name"
               required
+              :valid="!$v.editedAddress.lastname.$error"
+              :error-message="$t('Field is required.')"
               class="form__element form__element--half form__element--half-even"
             />
             <SfInput
@@ -31,6 +39,8 @@
               name="streetName"
               label="Street Name"
               required
+              :valid="!$v.editedAddress.streetName.$error"
+              :error-message="$t('Field is required.')"
               class="form__element"
             />
             <SfInput
@@ -38,6 +48,8 @@
               name="apartment"
               label="House/Apartment number"
               required
+              :valid="!$v.editedAddress.apartment.$error"
+              :error-message="$t('Field is required.')"
               class="form__element"
             />
             <SfInput
@@ -45,20 +57,27 @@
               name="city"
               label="City"
               required
+              :valid="!$v.editedAddress.city.$error"
+              :error-message="$t('Field is required.')"
               class="form__element form__element--half"
             />
             <SfInput
               v-model="editedAddress.state"
               name="state"
               label="State/Province"
-              required
               class="form__element form__element--half form__element--half-even"
             />
             <SfInput
-              v-model="editedAddress.zipCode"
-              name="zipCode"
+              v-model="editedAddress.postcode"
+              name="postcode"
               label="Zip-code"
               required
+              :valid="!$v.editedAddress.postcode.$error"
+              :error-message="
+                !$v.editedAddress.postcode.required
+                  ? $t('Field is required.')
+                  : $t('Zip-code must have at least 3 letters.')
+              "
               class="form__element form__element--half"
             />
             <SfSelect
@@ -66,6 +85,8 @@
               name="country"
               label="Country"
               required
+              :valid="!$v.editedAddress.country.$error"
+              :error-message="$t('Field is required.')"
               class="sf-select--underlined form__select form__element form__element--half form__element--half-even"
             >
               <SfSelectOption
@@ -77,10 +98,9 @@
               </SfSelectOption>
             </SfSelect>
             <SfInput
-              v-model="editedAddress.phoneNumber"
-              name="phone"
+              v-model="editedAddress.telephone"
+              name="telephone"
               label="Phone number"
-              required
               class="form__element"
             />
             <SfButton class="form__button" @click="updateAddress">
@@ -97,19 +117,18 @@
           <transition-group tag="div" name="fade" class="shipping-list">
             <div
               v-for="(address, key) in addresses"
-              :key="address.streetName + address.apartment"
+              :key="address.street.join(' ')"
               class="shipping"
             >
               <div class="shipping__content">
                 <p class="shipping__address">
-                  <span class="shipping__client-name">{{ address.firstName }} {{ address.lastName }}</span><br>
-                  {{ address.streetName }} {{ address.apartment }}<br>{{
-                    address.zipCode
-                  }}
-                  {{ address.city }},<br>{{ address.country }}
+                  <span class="shipping__client-name">{{ address.firstname }} {{ address.lastname }}</span><br>
+                  {{ address.street.join(' ') }}<br>
+                  {{ address.postcode }}
+                  {{ address.city }},<br>{{ getCountryById(address.country_id) }}
                 </p>
                 <p class="shipping__address">
-                  {{ address.phoneNumber }}
+                  {{ address.telephone }}
                 </p>
               </div>
               <div class="shipping__actions">
@@ -143,6 +162,8 @@
 </template>
 
 <script>
+import { required, minLength } from 'vuelidate/lib/validators';
+import { unicodeAlpha, unicodeAlphaNum } from '@vue-storefront/core/helpers/validators';
 import { SfTabs, SfInput, SfButton, SfSelect, SfIcon } from '@storefront-ui/vue';
 const Countries = require('@vue-storefront/i18n/resource/countries.json')
 
@@ -154,15 +175,15 @@ export default {
       editAddress: false,
       editedAddressIndex: -1,
       editedAddress: {
-        firstName: '',
-        lastName: '',
+        firstname: '',
+        lastname: '',
         streetName: '',
         apartment: '',
         city: '',
         state: '',
-        zipCode: '',
+        postcode: '',
         country: '',
-        phoneNumber: ''
+        telephone: ''
       },
       countries: Countries
     }
@@ -171,6 +192,238 @@ export default {
     addresses () {
       return this.$store.state.user.current.addresses
     }
+  },
+  methods: {
+    changeAddress (index) {
+      this.clearFields()
+      this.$v.$reset();
+      if (index > -1) {
+        this.editedAddress.firstname = this.addresses[index].firstname
+        this.editedAddress.lastname = this.addresses[index].lastname
+        this.editedAddress.streetName = this.addresses[index].street[0]
+        this.editedAddress.apartment = this.addresses[index].street[1]
+        this.editedAddress.city = this.addresses[index].city
+        this.editedAddress.state = this.addresses[index].region.region
+        this.editedAddress.postcode = this.addresses[index].postcode
+        this.editedAddress.country = this.getCountryById(this.addresses[index].country_id)
+        this.editedAddress.telephone = this.addresses[index].telephone
+      }
+      this.editedAddressIndex = index
+      this.editAddress = true;
+    },
+    clearFields () {
+      this.editedAddress.firstname = ''
+      this.editedAddress.lastname = ''
+      this.editedAddress.streetName = ''
+      this.editedAddress.apartment = ''
+      this.editedAddress.city = ''
+      this.editedAddress.state = ''
+      this.editedAddress.postcode = ''
+      this.editedAddress.country = ''
+      this.editedAddress.telephone = ''
+    },
+    getCountryById (id) {
+      let countryObject = this.countries.filter(country => country.code === id)
+      return countryObject.length > 0 ? countryObject[0].name : id
+    },
+    updateAddress () {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.$store.dispatch('notification/spawnNotification', {
+          type: 'error',
+          message: this.$t('Please fix the validation errors'),
+          action1: { label: this.$t('OK') }
+        });
+        return;
+      }
+
+      let userDataToUpdate = this.$store.state.user.current;
+      let addressToUpdate = {
+        firstname: this.editedAddress.firstname,
+        lastname: this.editedAddress.lastname,
+        street: [this.editedAddress.streetName, this.editedAddress.apartment],
+        city: this.editedAddress.city,
+        ...(this.editedAddress.state ? { region: { region: this.editedAddress.state } } : {}),
+        country_id: this.countries.find(country => country.name === this.editedAddress.country).code || this.editedAddress.country,
+        postcode: this.editedAddress.postcode,
+        ...(this.editedAddress.telephone ? { telephone: this.editedAddress.telephone } : {})
+      }
+
+      if (this.editedAddressIndex > -1) {
+        userDataToUpdate.addresses[this.editedAddressIndex] = Object.assign({}, userDataToUpdate.addresses[this.editedAddressIndex], addressToUpdate)
+      } else {
+        userDataToUpdate.addresses.push(addressToUpdate)
+      }
+
+      this.$store.dispatch('user/update', { customer: userDataToUpdate }).then(() => {
+        this.clearFields();
+        this.editedAddressIndex = -1;
+        this.editAddress = false;
+      })
+    },
+    deleteAddress (index) {
+      let userDataToUpdate = this.$store.state.user.current;
+      userDataToUpdate.addresses.splice(index, 1)
+      this.$store.dispatch('user/update', { customer: userDataToUpdate })
+    }
+  },
+  validations: {
+    editedAddress: {
+      firstname: {
+        required,
+        minLength: minLength(2),
+        unicodeAlpha
+      },
+      lastname: {
+        required
+      },
+      streetName: {
+        required,
+        unicodeAlphaNum
+      },
+      apartment: {
+        required,
+        unicodeAlphaNum
+      },
+      city: {
+        required,
+        unicodeAlpha
+      },
+      postcode: {
+        required,
+        minLength: minLength(3)
+      },
+      country: {
+        required
+      }
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "~@storefront-ui/vue/styles";
+@mixin for-mobile {
+  @media screen and (max-width: $desktop-min) {
+    @content;
+  }
+}
+@mixin for-desktop {
+  @media screen and (min-width: $desktop-min) {
+    @content;
+  }
+}
+.form {
+  @include for-desktop {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  &__element {
+    margin-bottom: $spacer-extra-big;
+    @include for-desktop {
+      flex: 0 0 100%;
+    }
+    &--half {
+      @include for-desktop {
+        flex: 1 1 50%;
+      }
+      &-even {
+        @include for-desktop {
+          padding-left: $spacer-extra-big;
+        }
+      }
+    }
+  }
+  &__select {
+    ::v-deep .sf-select__selected {
+      padding: 5px 0;
+    }
+  }
+  &__button {
+    width: 100%;
+    @include for-desktop {
+      width: auto;
+    }
+  }
+}
+.message {
+  margin: 0 0 $spacer-extra-big 0;
+  font-family: $body-font-family-primary;
+  font-weight: $body-font-weight-primary;
+  line-height: 1.6;
+  font-size: $font-size-regular-mobile;
+  @include for-desktop {
+    font-size: $font-size-regular-desktop;
+  }
+}
+.shipping-list {
+  margin-bottom: $spacer-extra-big;
+}
+.shipping {
+  display: flex;
+  padding: $spacer-big 0;
+  border-top: 1px solid $c-light;
+  &:last-child {
+    border-bottom: 1px solid $c-light;
+  }
+  &__content {
+    flex: 1;
+    color: $c-text;
+    font-size: $font-size-small-mobile;
+    font-weight: 300;
+    line-height: 1.6;
+    @include for-desktop {
+      font-size: $font-size-small-desktop;
+    }
+  }
+  &__actions {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+    @include for-desktop {
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-end;
+    }
+  }
+  &__button-delete {
+    background-color: $c-light;
+    color: $c-text-muted;
+    @include for-desktop {
+      margin-left: $spacer-big;
+    }
+  }
+  &__address {
+    margin: 0 0 $spacer-big 0;
+    &:last-child {
+      margin: 0;
+    }
+  }
+  &__client-name {
+    font-size: $font-size-regular-desktop;
+    font-weight: 500;
+  }
+}
+.action-button {
+  width: 100%;
+  @include for-desktop {
+    width: auto;
+  }
+}
+.tab-orphan {
+  @include for-mobile {
+    ::v-deep .sf-tabs {
+      &__title {
+        display: none;
+      }
+      &__content {
+        border: 0;
+        padding: 0;
+      }
+    }
+  }
+}
+</style>
