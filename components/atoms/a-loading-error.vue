3a1359f8c37c3c0d14f1e78e43919f8d59a77dbc
Paweł Smyrek <psmyrek@divante.pl>
2020-03-30 09:59:15 +0200

    275: Move LoadingError and LoadingSpinner to atoms

---
+++ components/atoms/a-loading-error.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="a-loading-error">
+    <SfAlert
+      :message="OnlineOnly
+        ? $t('An error occured while loading the component.')
+        : $t('Component couldn\'t be loaded due to lack of network connectivity.')"
+      type="danger"
+    />
+    <SfButton
+      v-if="OnlineOnly"
+      class="sf-button--text color-secondary"
+      @click="$emit('reload')"
+    >
+      {{ $t('Try again') }}
+    </SfButton>
+    <SfButton
+      class="sf-button--text color-secondary"
+      @click="$emit('close')"
+    >
+      {{ $t('Close') }}
+    </SfButton>
+  </div>
+</template>
+
+<script>
+import VueOfflineMixin from 'vue-offline/mixin';
+import { SfButton, SfAlert } from '@storefront-ui/vue';
+
+export default {
+  name: 'ALoadingError',
+  components: { SfButton, SfAlert },
+  mixins: [VueOfflineMixin],
+  mounted () {
+    this.$on('online', () => this.$emit('reload'));
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.a-loading-error {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  margin: auto;
+  .sf-alert {
+    flex-basis: 100%;
+    justify-content: center;
+  }
+  .sf-button {
+    margin: 0 1em;
+  }
+}
+</style>
