09ba9d941fe5b28720542e9c321336b8da671c53
Tomasz Kikowski <tomaszkikowski@gmail.com>
2020-04-17 09:17:41 +0200

    Adopt PDP to SFUI 0.7

--- components/organisms/o-product-details.vue
+++ components/organisms/o-product-details.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="o-product-details" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+  <div class="o-product-details product" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
     <meta itemprop="priceCurrency" :content="$store.state.storeView.i18n.currencyCode">
     <meta itemprop="price" :content="parseFloat(product.price_incl_tax).toFixed(2)">
     <meta itemprop="availability" :content="availability">
@@ -9,19 +9,20 @@
       :gallery="gallery"
       :configuration="productConfiguration"
     />
-    <div class="description">
-      <SfSticky>
-        <MProductShortInfo
-          :product="product"
-          :custom-options="productCustomOptions"
-          :reviews="reviews"
-        />
-        <ATextAction
-          v-if="sizeOption"
-          class="text-action"
-          :text="$t('Size guide')"
-          @click="openSizeGuide"
-        />
+    <div class="product__info">
+      <MProductShortInfo
+        :product="product"
+        :custom-options="productCustomOptions"
+        :reviews="reviews"
+      />
+      <SfButton
+        v-if="sizeOption"
+        @click.native="openSizeGuide"
+        class="sf-button--text desktop-only product__guide"
+      >
+        {{ $t('Size guide') }}
+      </SfButton>
+      <div>
         <MProductOptionsConfigurable
           v-if="product.type_id =='configurable'"
           :product="product"
@@ -44,12 +45,12 @@
           :product="product"
           :stock="productStock"
         />
-        <MProductAdditionalInfo
-          :product="product"
-          :reviews="reviews"
-          :attributes="productAttributes"
-        />
-      </SfSticky>
+      </div>
+      <MProductAdditionalInfo
+        :product="product"
+        :reviews="reviews"
+        :attributes="productAttributes"
+      />
     </div>
   </div>
 </template>
@@ -57,8 +58,7 @@
 import get from 'lodash-es/get'
 import config from 'config';
 import { mapGetters, mapActions } from 'vuex';
-import { SfSticky } from '@storefront-ui/vue';
-import ATextAction from 'theme/components/atoms/a-text-action';
+import { SfButton } from '@storefront-ui/vue';
 import MProductGallery from 'theme/components/molecules/m-product-gallery';
 import MProductShortInfo from 'theme/components/molecules/m-product-short-info';
 import MProductCallToAction from 'theme/components/molecules/m-product-call-to-action';
@@ -71,8 +71,7 @@ import { ModalList } from 'theme/store/ui/modals';
 
 export default {
   components: {
-    SfSticky,
-    ATextAction,
+    SfButton,
     MProductGallery,
     MProductShortInfo,
     MProductCallToAction,
@@ -165,23 +164,25 @@ export default {
 <style lang="scss" scoped>
 @import "~@storefront-ui/shared/styles/helpers/breakpoints";
 
-.o-product-details {
+.product {
   @include for-desktop {
     display: flex;
   }
-}
-.description {
-  flex: 1;
-  padding: 0 var(--spacer-xl);
-  @include for-desktop {
-    margin-left: calc(var(--spacer-xl) * 5);
+  &__info {
+    margin: var(--spacer-sm) auto var(--spacer-xs);
+    @include for-desktop {
+      max-width: 32.625rem;
+      margin: 0 0 0 7.5rem;
+    }
+  }
+  &__guide,
+  &__compare,
+  &__save {
+    display: block;
+    margin: var(--spacer-xl) 0 var(--spacer-base) auto;
   }
 }
-.text-action {
-  @include for-desktop {
-    justify-content: flex-end;
-  }
-}
+
 .section {
   border-bottom: 1px solid #f1f2f3;
   padding-bottom: 10px;
