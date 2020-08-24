06dd974eca68f09c5a594da4f06b5312e06bd0b5
sakthiraj93 <sakthiraj93@gmail.com>
2020-08-24 17:25:46 +0530

    Feature ( Support Block ) : Home Page Support Block Section Design

--- components/molecules/m-support-block.vue
+++ components/molecules/m-support-block.vue
@@ -1,42 +1,38 @@
 <template>
   <div class="support-block">     
-    <div class="parent_container">
-      <div class="content d-flex">
-        <div class="child_container_1" >
+    <div class="container">
+      <div class="support-block--content d-flex">
+        <div class="support-block--item d-flex align-start" >
            <img
-            :src="'/assets/placeholder.jpg'"
-            class="image_contact_1" />
+            :src="'/assets/images/support-block-querries.jpg'"
+            class="support-block-querries" />
             <div> 
-               <div class="text_contact_1">Recherchez-vous une catégorie particulière de produits?</div>
-                <SfButton class="rectangle_button_1" :link="link">
+               <h3>Recherchez-vous une catégorie particulière de produits?</h3>
+                <SfButton class="btn-primary-outline-gray" :link="link">
                     Voir nos catégories
                   </SfButton> 
             </div>
         </div>
-        <div class="rectangle_small"> </div>
-         <div class="child_container_2">
+         <div class="support-block--item d-flex align-start">
              <img
-            :src="'/assets/placeholder.jpg'"
-            class="image_contact_1" />
-            <div class="inner_text_container_1">
-               <div class="text_contact_2">Avez-vous une question ?</div>
-               <div class="text_contact_3">Notre équipe de service client est à votre écoute.</div>
-               <div class="text_contact_4">09 70 72 60 00</div>
-               <div class="text_contact_5">Lundi à Vendredi(8h-18h)</div>
-               <div class="text_contact_6">Ou écrivez un mail à <b>contact@capitools.com</b></div>
+            :src="'/assets/images/support-block-contact.jpg'"
+            class="support-block-contact" />
+            <div class="">
+               <h3>Avez-vous une question ?</h3>
+               <p>Notre équipe de service client est à votre écoute.</p>
+               <p><b>09 70 72 60 00</b> <small>(Appel non surtaxé)</small> Lundi à Vendredi(8h-18h)</p>
+               <p>Ou écrivez un mail à <b>contact@capitools.com</b></p>
             </div>
         </div>
-        <div class="rectangle_small"> </div>
-         <div class="child_container_3">
-            <img
-            :src="'/assets/placeholder.jpg'"
-            class="image_contact_1" />
-            <div class="inner_text_container_1">
-               <div class="text_contact_2">Avez-vous une question ?</div>
-               <div class="text_contact_3">Notre équipe de service client est à votre écoute.</div>
-               <div class="text_contact_4">09 70 72 60 00</div>
-               <div class="text_contact_5">Lundi à Vendredi(8h-18h)</div>
-               <div class="text_contact_6">Ou écrivez un mail à <b>contact@capitools.com</b></div>
+         <div class="support-block--item d-flex align-start">
+           <img
+            :src="'/assets/images/support-block-contact.jpg'"
+            class="support-block-contact" />
+             <div class="">
+               <h3>Avez-vous une question ?</h3>
+               <p>Notre équipe de service client est à votre écoute.</p>
+               <p><b>09 70 72 60 00</b> <small>(Appel non surtaxé)</small> Lundi à Vendredi(8h-18h)</p>
+               <p>Ou écrivez un mail à <b>contact@capitools.com</b></p>
             </div>
         </div>
       </div>
@@ -57,139 +53,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.parent_container
-{
-  height: 303px;
-  width: 1920px;
-}
-.child_container_1
-{
-  height: 194px;
-  width: 380px;
-  display: flex;
-   border-radius: 3px;
-  margin-top: 50px;
-  margin-left: 355px;
-  margin-bottom: 60px;
-}
 
-.child_container_2
-{
-  height: 194px;
-  width: 380px;
-  display: flex;
-   border-radius: 3px;
-  margin-top: 50px;
-  margin-bottom: 60px;
-}
-.child_container_3
-{
- height: 194px;
-  width: 380px;
-  display: flex;
-   border-radius: 3px;
-  margin-top: 50px;
-  margin-bottom: 60px;
-}
-
-.inner_text_container_1
-{
-  height: 28px;
-  width: 253px;
-  margin-left: 20px;
-}
-
-.image_contact_1
-{
-  height: 183px;
-  width: 146px;
-} 
-.text_contact_1
-{
-  height: 112px;
-  width: 196px;
-  color: #222222;
-  font-family: Poppins;
-  font-size: 20px;
-  letter-spacing: 0;
-  line-height: 30px;
-  margin-left: 20px;
-}
-.text_contact_2 
-{ 
-  color: #222222;
-  font-family: Poppins;
-  font-size: 20px;
-  letter-spacing: 0;
-  line-height: 30px; 
-}
-.text_contact_3 
-{ 
-  color: #484848;
-  font-family: Poppins;
-  font-size: 14px;
-  letter-spacing: 0;
-  line-height: 21px;
-  margin-top: 12px;
-}
-.text_contact_4
- {
-  height: 20px;
-  width: 96px;
-  color: #222222;
-  font-family: Poppins;
-  font-size: 14px;
-  font-weight: bold;
-  letter-spacing: 0;
-  line-height: 21px;
-  margin-top: 10px;
-}
-.text_contact_5 
-{
-  height: 20px;
-  width: 172px;
-  color: #222222;
-  font-family: Poppins;
-  font-size: 14px;
-  font-weight: 300;
-  letter-spacing: 0;
-  line-height: 21px;
-  margin-top: 5px;
-}
-.text_contact_6 
-{
-  height: 40px;
-  width: 180px;
-  color: #222222;
-  font-family: Poppins;
-  font-size: 14px;
-  font-weight: bold;
-  letter-spacing: 0;
-  line-height: 21px;
-  margin-top: 10px;
-}
-.rectangle_small {
-  height: 194px;
-  width: 2px;
-  background-color: #E3E6EA;
-  margin-left: 30px;
-  margin-top: 50px;
-  margin-right: 30px;
-} 
-
-.rectangle_button_1
-{
-  box-sizing: border-box;
-  height: 41px;
-  width: 171px;
-  border: 1px solid #E3E6EA;
-  border-radius: 3px;
-  background-color: #FFFFFF;
-  box-shadow: 0 5px 10px 0 rgba(0,0,0,0.08);
-  --button-color:#3B78DD;
-  --button-font-size:14px;
-  --button-font-family:Poppins;
-  --button-font-line-height:21px;
-    margin-left: 20px;
-}
 </style>
