ec1107bf27fd194b9b45f802551dbb8924ed751c
maheshwaran-1343 <maheswaran@superbotics.in>
2020-08-25 14:47:11 +0530

    Feature ( Product Carousel ) : Show price , sku , add-to-card in Product Carousel

--- components/molecules/m-product-carousel.vue
+++ components/molecules/m-product-carousel.vue
@@ -7,38 +7,71 @@
     }"
   >
     <SfCarouselItem v-for="(product, i) in carouselProducts" :key="i">
-      <SfProductCard
-        :title="product.title"
-        :image="product.image"
-        :regular-price="product.price.regular"
-        :special-price="product.price.special"
-        :max-rating="product.rating.max"
-        :score-rating="product.rating.score"
+      <SfProductCard 
+        :image="product.image"  
         :link="product.link"
         :wishlist-icon="false"
         link-tag="router-link"
-      />
+      >
+          <template #title>
+            <h3 class="sf-product-card__title">
+                {{ product.title }}
+            </h3>
+            <p>
+                Ref : {{ product.sku }}
+            </p>
+            <AProductRating
+              @click="openReviewsTab"
+              :reviews="reviews"
+              >
+            </AProductRating> 
+            <SfPrice
+              class="sf-product-card__price"
+              :regular="product.price.regular"
+              :special="product.price.special" 
+            />     
+          </template> 
+          <template #price>
+            <AAddToCart
+              class="sf-add-to-cart__button"
+              :qty="qty"
+              :product="product.obj_product"
+              :disabled="addToCartDisabled"
+            />  
+          </template>
+       </SfProductCard>
     </SfCarouselItem>
   </SfCarousel>
 </template>
 <script>
-import { SfProductCard, SfCarousel } from '@storefront-ui/vue';
+import { SfProductCard, SfCarousel ,   SfRating, SfPrice, } from '@storefront-ui/vue';
 import { htmlDecode } from '@vue-storefront/core/filters';
 import config from 'config';
 import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 import { formatProductLink } from '@vue-storefront/core/modules/url/helpers';
 import { productThumbnailPath } from '@vue-storefront/core/helpers';
 import { prepareCategoryProduct } from 'theme/helpers';
+import AProductRating from 'theme/components/atoms/a-product-rating';
+import AAddToCart from 'theme/components/atoms/a-add-to-cart';
+
 export default {
   name: 'MProductCarousel',
   components: {
     SfCarousel,
-    SfProductCard
+    SfProductCard,
+    SfRating,
+    SfPrice,
+    AProductRating,
+    AAddToCart
   },
   props: {
     products: {
       type: Array,
       default: () => []
+    },
+    qty: {
+      type: Number,
+      default: 1
     }
   },
   computed: {
