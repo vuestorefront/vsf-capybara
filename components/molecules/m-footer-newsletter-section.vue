7eb0191e6651167af20af8ae4db21836640146a5
maheshwaran-1343 <maheswaran@superbotics.in>
2020-09-18 20:49:48 +0530

    Feature ( Mailchimp Integration ) : Mailchimp integration in home page , sing-up page and checkout page

--- components/molecules/m-footer-newsletter-section.vue
+++ components/molecules/m-footer-newsletter-section.vue
@@ -9,35 +9,39 @@
       </p>
       <p>
         Sign Up for Tips, Reviews & Special Offers
-      </p>
-      <div class="d-flex align-start">
-        <template>
-          <SfInput
-              v-model="value"
-              :type="type"
-              :name="name"
-              :valid="valid"
-              :error-message="errorMessage"
+      </p> 
+        <form @submit.prevent="subscribeExtend" class="form">
+          <div class="d-flex align-start">
+            <template>
+              <SfInput
+              v-model="email"
+              name="email"  
+              :valid="!$v.email.$error"
               :placeholder="placeholder"
-            />
-          <SfButton 
-            class="btn-primary"
-            link="#"
-            @click="subscribe"
-          >
-            Register
-          </SfButton>
-        </template>
-      </div>
+              :error-message="
+                !$v.email.required
+                  ? $t('Field is required.')
+                  : $t('Please provide valid e-mail address.')
+              "
+              class="form__element"
+            /> 
+            <SfButton class="btn-primary">
+              {{ $t("Register") }}
+            </SfButton>
+            </template>
+          </div>
+        </form>  
     </div>
   </div>
 </template>
 
 <script>
-import { SfInput,  SfButton } from "@storefront-ui/vue";
-import { Newsletter } from '@vue-storefront/core/modules/newsletter/components/Newsletter'
+import i18n from '@vue-storefront/i18n'; 
+import Subscribe from '@vue-storefront/core/modules/newsletter/mixins/Subscribe'
+import { required, email } from 'vuelidate/lib/validators';
+import { SfInput,  SfButton } from "@storefront-ui/vue"; 
 export default {
-  mixins: [Newsletter],
+  mixins: [Subscribe],
   components: {
     SfInput, SfButton
   },
@@ -49,7 +53,30 @@ export default {
       errorMessage: "Required.",
       valid: true,
       placeholder: "Enter your email",
+      email: ''
     };
   },
+  methods: {
+    subscribeExtend () {
+      this.$v.$touch();
+      if (this.$v.$invalid) return
+      this.subscribe(this.onSuccesfulSubmission)
+    },
+    onSuccesfulSubmission (isSubscribed) {
+      if (isSubscribed) {
+        this.$store.dispatch('notification/spawnNotification', {
+          type: 'success',
+          message: i18n.t('You have been successfully subscribed to our newsletter!'),
+          action1: { label: i18n.t('OK') }
+        })
+      } 
+    }
+  }, 
+   validations: {
+    email: {
+      required,
+      email
+    }
+  }
 };
 </script>
