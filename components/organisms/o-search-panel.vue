09ce61de9dff9e035e5aebfdd01a5ad6016db6b4
Aishwary Shrivastava <aishwary.shrivastava@hotwaxsystems.com>
2020-09-19 11:39:25 +0530

    Added margin top for headings (#478)

--- components/organisms/o-search-panel.vue
+++ components/organisms/o-search-panel.vue
@@ -198,6 +198,10 @@ export default {
     &__title {
       padding: 0;
       line-height: 3;
+      margin-top: var(--spacer-base);
+      @include for-desktop {
+        margin-top: 0;
+      }
       justify-content: start;
     }
     &__listing {
