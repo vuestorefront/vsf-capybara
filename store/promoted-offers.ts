77fd74ec443e5cc9954dfce52fa16310bd09e67d
kaitco13 <kaitcolbert@hotmail.com>
2021-04-01 05:37:41 +0000

    configurable promoted images

--- store/promoted-offers.ts
+++ store/promoted-offers.ts
@@ -1,4 +1,6 @@
 import { Logger } from '@vue-storefront/core/lib/logger'
+import { TaskQueue } from '@vue-storefront/core/lib/sync'
+import config from 'config';
 
 export default interface PromotedOffersState {
   banners: {
@@ -8,6 +10,24 @@ export default interface PromotedOffersState {
     menuAsideBanners: any[]
   },
   headImage: any[]
+}
+
+const getImages = async(url): Promise<any> => {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED="0"
+  try {
+    const task = await TaskQueue.execute({ url,
+      payload: {
+        method: 'GET',
+        headers: {
+         'Content-Type': 'application/json',
+        },
+        mode: 'cors'
+      },
+    })
+    return task.result;
+  } catch (err) {
+    console.error('error', err)
+  }
 }
 
 export const promotedStore = {
@@ -28,22 +48,24 @@ export const promotedStore = {
     getHeadImage: state => state.headImage
   },
   actions: {
-    async updatePromotedOffers ({ commit, rootState }, data) {
+    async updatePromotedOffers ({ commit, rootState }) {
       let promotedBannersResource = rootState.storeView && rootState.storeView.storeCode ? `banners/${rootState.storeView.storeCode}_promoted_offers` : `promoted_offers`
       try {
-        // Workaround to get jest --watch to work so don't change the import sting to a template string
-        const promotedOffersModule = await import(/* webpackChunkName: "vsf-promoted-offers-[request]" */ 'theme/resource/' + promotedBannersResource + '.json')
-        commit('updatePromotedOffers', promotedOffersModule)
+        // query the api for promoted images
+        console.log('storeview: ',rootState.storeView)
+        let url = config.images.baseUrl+"promoted"
+        const images = await getImages(url)
+        commit('updatePromotedOffers', images)
       } catch (err) {
         Logger.debug('Unable to load promotedOffers' + err)()
       }
     },
-    async updateHeadImage ({ commit, rootState }, data) {
-      let mainImageResource = rootState.storeView && rootState.storeView.storeCode ? `banners/${rootState.storeView.storeCode}_main-images` : `main-images`
+    async updateHeadImage ({ commit }) {
       try {
-        // Workaround to get jest --watch to work so don't change the import sting to a template string
-        const imageModule = await import(/* webpackChunkName: "vsf-head-img-[request]" */ 'theme/resource/' + mainImageResource + '.json')
-        commit('SET_HEAD_IMAGE', imageModule.images)
+        // query the api for banner images
+        let url = config.images.baseUrl+"head"
+        const images = await getImages(url)
+        commit('SET_HEAD_IMAGE', images.images)
       } catch (err) {
         Logger.debug('Unable to load headImage' + err)()
       }
