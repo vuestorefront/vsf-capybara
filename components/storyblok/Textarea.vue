4b494ca34b101bbc64de278c943a342b430123e8
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-11 23:33:33 +0300

    Refs #24341: Storyblok integration - Textarea - add styles/classes properties support, tweak ul list displaying

--- components/storyblok/Textarea.vue
+++ components/storyblok/Textarea.vue
@@ -1,18 +1,31 @@
 <template>
-  <div>
+  <div
+    class="storyblok-textarea"
+    :class="cssClasses"
+    :style="styles"
+  >
     <sb-rich-text v-if="item.content" :text="item.content" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Blok } from 'src/modules/vsf-storyblok-module/components'
-export default {
+
+export default Blok.extend({
   name: 'Textarea',
-  extends: Blok,
   computed: {
     itemValue () {
       return this.item;
     }
   }
-}
+});
 </script>
+
+<style lang="scss" scoped>
+.storyblok-textarea {
+  ::v-deep ul, ol, dl {
+    display: inline-block;
+    text-align: left;
+  }
+}
+</style>
