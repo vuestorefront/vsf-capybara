bbbd9f7c668789532ce9cb870c606eb1e12123af
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-13 14:08:36 +0300

    Refs #24655: Storyblok integration - HomepageIntroSection - fix and add mobile styles

--- components/storyblok/HomepageIntroSection.vue
+++ components/storyblok/HomepageIntroSection.vue
@@ -1,59 +1,76 @@
 <template>
   <div
-    class="storyblok-homepage-intro-section _intro-row"
+    class="storyblok-homepage-intro-section"
     :class="cssClasses"
     :style="{ backgroundColor: item.background_color }"
   >
-    <div class="_intro-row">
-      <div class="_intro-column _content">
-        <h2>
-          <sb-rich-text :text="item.textarea" />
-        </h2>
-        <p class="_subtitle" v-if="item.subtitle_content">
-          {{ item.subtitle_content }}
-        </p>
-        <div class="_content-btn">
+    <div class="_intro-column _image">
+      <SfImage
+        :src="srcSet"
+        :picture-breakpoint="768"
+      />
+    </div>
+
+    <div class="_intro-column _content">
+      <div class="_title-block">
+        <SfHeading
+          :level="2"
+          :subtitle="item.subtitle"
+        >
+          <template #title>
+            <h2
+              class="sf-heading__title sf-heading__title--h2"
+              v-html="nl2br(item.title)"
+            />
+          </template>
+
+          <template #subtitle="{ subtitle }">
+            <h3
+              class="_subtitle sf-heading__title sf-heading__title--h3"
+              v-if="subtitle"
+            >
+              {{ subtitle }}
+            </h3>
+          </template>
+        </SfHeading>
+
+        <div class="_button-row">
           <SfButton
-            :link="item.button_link"
-            :target="_self"
+            :link="link"
+            class="_button"
+            @click="openLink"
           >
-            {{ item.button_text_required }}
+            {{ item.button_text }}
           </SfButton>
         </div>
-      </div>
-      <div class="_intro-column _image">
-        <SfImage
-          :src="srcSet"
-          :picture-breakpoint="768"
-        />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Blok } from 'src/modules/vsf-storyblok-module/components';
-import { SfImage } from '@storefront-ui/vue';
-import { SfButton } from '@storefront-ui/vue';
+import { localizedRoute } from '@vue-storefront/core/lib/multistore';
+import { nl2br } from 'src/modules/budsies';
 
-interface SrcSetValue {
-  desktop?: { url: string },
-  mobile?: { url: string }
-}
+import { Blok } from 'src/modules/vsf-storyblok-module/components';
+import {
+  SfImage,
+  SfButton,
+  SfHeading
+} from '@storefront-ui/vue';
+
+import SrcSetValue from './src-set-value.interface';
 
 export default Blok.extend({
   name: 'StoryblokHomepageIntroSection',
   components: {
     SfImage,
-    SfButton
+    SfButton,
+    SfHeading
   },
   computed: {
     link (): string {
-      return this.item.link_url.url;
-    },
-    linkTarget (): string {
-      return this.item.target_blank ? '_blank'
-        : '_self';
+      return this.item.button_link.url;
     },
     srcSet (): SrcSetValue | string {
       if (!this.item.mobile_image.filename) {
@@ -71,61 +88,81 @@ export default Blok.extend({
 
       return srcSet;
     }
+  },
+  methods: {
+    nl2br (text: string): string {
+      return nl2br(text);
+    },
+    openLink (): void {
+      this.$router.push(localizedRoute(this.link));
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
+@import "~@storefront-ui/shared/styles/helpers/breakpoints";
+
 .storyblok-homepage-intro-section {
   position: relative;
 
-  ._intro-row {
-    position: relative;
+  ._content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+    padding: 1em;
+    text-align: inherit;
+    z-index: 10;
 
-    ._content {
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-      justify-content: center;
-      order: 2;
-      padding: 0 5% 0 55%;
-      position: absolute;
-      text-align: inherit;
-      width: 100%;
-      z-index: 10;
+    ._title-block {
+      .sf-heading__title {
+        line-height: 1.2;
+      }
 
       ._subtitle {
-        font-size: 1.2em;
-      }
-
-      h2 {
-        font-size: 1.8em;
-        text-align: inherit;
-        margin: 0;
-
-        p {
-          font-size: inherit;
-          font-weight: inherit;
-          line-height: 1.2em;
-          margin: 0;
-        }
-      }
-
-      &.-desktop {
-        display: none;
+        margin-top: 1em;
       }
     }
 
-    ._image {
-      img {
-        width: 100%;
-      }
+    ._button-row {
+      margin-top: 2em;
+      text-align: center;;
     }
 
     ._button {
-      margin-top: 2em;
-      text-align: center;
+      display: inline-block;
     }
+
+    &.-desktop {
+      display: none;
+    }
+  }
+
+  ._image {
+    img {
+      width: 100%;
+    }
+  }
+
+  @media (min-width: $tablet-min) {
+    ._content {
+      padding: 0 5% 0 55%;
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      ._title-block {
+        .sf-heading__title {
+          text-align: left;
+        }
+
+        ._button-row {
+          text-align: left;
+        }
+      }
+    }
+
   }
 }
 </style>
