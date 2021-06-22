98b2187b0fccccfa428ef0c051cd3689d0c831b5
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-22 22:32:39 +0300

    Refs #24682: Storyblok - Slider - prepare optimized images for gallery

--- components/storyblok/Slider.vue
+++ components/storyblok/Slider.vue
@@ -6,33 +6,82 @@
 </template>
 
 <script lang="ts">
+import { VueConstructor } from 'vue';
+import { mapGetters } from 'vuex';
+
+import { InjectType } from 'src/modules/shared';
+import { ComponentWidthCalculator } from 'src/modules/vsf-storyblok-module';
+
 import MZoomGallery from 'theme/components/molecules/m-zoom-gallery.vue';
 import ZoomGalleryImage from 'theme/interfaces/zoom-gallery-image.interface';
 
 import { Blok } from 'src/modules/vsf-storyblok-module/components'
 import SliderData from './interfaces/slider-data.interface';
 import { ThumbnailsPosition } from './interfaces/thumbnails-position.value';
+import generateBreakpointsSpecs from './generate-breakpoints-specs';
+import generateImageSourcesList from './generate-image-sources-list';
+import getResizedImageUrl from './get-resized-image-url';
 
-export default Blok.extend({
+interface InjectedServices {
+  componentWidthCalculator: ComponentWidthCalculator
+}
+
+export default (Blok as VueConstructor<InstanceType<typeof Blok> & InjectedServices>).extend({
   name: 'Slider',
   components: {
     MZoomGallery
   },
+  inject: {
+    componentWidthCalculator: { default: undefined }
+  } as unknown as InjectType<InjectedServices>,
   computed: {
+    ...mapGetters({
+      supportsWebp: 'storyblok/supportsWebp'
+    }),
     itemData (): SliderData {
       return this.item as SliderData;
     },
     slides (): ZoomGalleryImage[] {
       const slides = [];
       for (const sliderItem of this.itemData.slider_items) {
+        let breakpointsSpecs = generateBreakpointsSpecs(
+          sliderItem.image.filename,
+          this.componentWidthCalculator,
+          sliderItem.mobile_image.filename
+        )
+
+        const stage = generateImageSourcesList(
+          breakpointsSpecs,
+          this.supportsWebp,
+          1
+        );
+
+        const thumbWidthCalculator = this.componentWidthCalculator.limitAllByPercent(20);
+
+        breakpointsSpecs = generateBreakpointsSpecs(
+          sliderItem.image.filename,
+          thumbWidthCalculator,
+          sliderItem.mobile_image.filename
+        )
+
+        const thumb = generateImageSourcesList(
+          breakpointsSpecs,
+          this.supportsWebp,
+          1
+        );
+
+        const big = getResizedImageUrl(
+          sliderItem.image.filename,
+          2730,
+          this.supportsWebp
+        )
+
         const slide: ZoomGalleryImage = {
-          thumb: sliderItem.image.filename,
-          stage: sliderItem.image.filename,
-          big: sliderItem.image.filename,
+          thumb,
+          stage,
+          big,
           alt: sliderItem.alt_tag,
-          title: sliderItem.title_tag,
-          link: sliderItem.link_url.url,
-          target: sliderItem.target_blank ? '_blank' : '_self'
+          title: sliderItem.title_tag
         };
 
         slides.push(slide);
