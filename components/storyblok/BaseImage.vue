154d31712cd79c7ddf5e4df32c5229355896b9ab
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-22 16:05:00 +0300

    Refs #24682: Storyblok - BaseImage - move sore features into the `BaseImage` component of the `budsies` module

--- components/storyblok/BaseImage.vue
+++ components/storyblok/BaseImage.vue
@@ -1,48 +1,12 @@
 <template>
-  <div
-    class="storyblok-base-image sf-image"
-    :class="{'-loading': !isLoaded}"
-    :style="styles"
+  <BudsiesBaseImage
+    :srcsets="sources"
+    :width="width"
+    v-bind="$attrs"
     v-on="$listeners"
   >
-    <div class="_placeholder" />
-
-    <div class="_image-wrapper">
-      <picture>
-        <source
-          v-for="source of sources"
-          :key="source.breakpoint"
-          :srcset="source.srcset.join(', ')"
-          :media="getMediaQuery(source.breakpoint)"
-        >
-        <img
-          v-show="defaultSrc"
-          ref="image"
-          :src="defaultSrc"
-          :srcset="defaultSrcSet"
-          v-bind="$attrs"
-          :width="width"
-          :loading="lazy ? 'lazy' : 'eager'"
-          @load="onLoad"
-        >
-      </picture>
-
-      <div v-if="hasOverlay" class="sf-image__overlay">
-        <slot />
-      </div>
-
-      <noscript inline-template>
-        <img
-        :src="defaultSrc"
-        :srcset="defaultSrcSet"
-        :alt="alt"
-        class="sf-image sf-image-loaded"
-        v-bind="$attrs"
-        :width="width"
-        />
-      </noscript>
-    </div>
-  </div>
+    <slot />
+  </BudsiesBaseImage>
 </template>
 
 <script lang="ts">
@@ -50,14 +14,10 @@ import Vue, { VueConstructor } from 'vue';
 import { mapGetters } from 'vuex'
 import { InjectKey } from 'vue/types/options';
 
-import generatePlaceholderStyles from './generate-placeholder-styles';
 import parseImageDimensions from './parse-image-dimensions';
 import ComponentWidthCalculator, { SizeValue } from 'src/modules/vsf-storyblok-module/component-width-calculator.service';
 
-interface SourceItem {
-  breakpoint: number,
-  srcset: string[]
-}
+import { BaseImage as BudsiesBaseImage, ImageSourceItem } from 'src/modules/budsies';
 
 interface InjectedServices {
   componentWidthCalculator: ComponentWidthCalculator
@@ -66,8 +26,11 @@ interface InjectedServices {
 type InjectType<T> = Record<keyof T, InjectKey | { from?: InjectKey, default?: any }>;
 
 export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
-  name: 'SfImage',
+  name: 'StoryblokBaseImage',
   inheritAttrs: false,
+  components: {
+    BudsiesBaseImage
+  },
   inject: {
     componentWidthCalculator: { default: undefined }
   } as unknown as InjectType<InjectedServices>,
@@ -80,26 +43,17 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
       type: String,
       default: undefined
     },
-    lazy: {
-      type: Boolean,
-      default: true
-    },
     width: {
       type: [String, Number],
       default: null
     }
   },
-  data () {
-    return {
-      isLoaded: false
-    };
-  },
   computed: {
     ...mapGetters({
       supportsWebp: 'storyblok/supportsWebp'
     }),
-    sources (): SourceItem[] {
-      const result: SourceItem[] = [];
+    sources (): ImageSourceItem[] {
+      const result: ImageSourceItem[] = [];
 
       const breakpoints = this.componentWidthCalculator.getBreakpoints();
       const widths = this.componentWidthCalculator.getWidths();
@@ -114,7 +68,7 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
       }
 
       let previousWidth = 0;
-      let sourceItem: SourceItem | undefined;
+      let sourceItem: ImageSourceItem | undefined;
 
       for (const size in widths) {
         const sizeKey = size as SizeValue;
@@ -138,14 +92,15 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
 
         const filters = this.supportsWebp ? '/filters:format(webp)' : '';
 
-        sourceItem = {
-          breakpoint: breakpoints[sizeKey],
-          srcset: []
-        };
-
         const [, resource] = src.split('//a.storyblok.com');
         let dimensions = parseImageDimensions(src);
         const ratio = dimensions.height / dimensions.width;
+
+        sourceItem = {
+          breakpoint: breakpoints[sizeKey],
+          aspectRatio: 1 / ratio,
+          srcset: []
+        };
 
         for (const density of [1, 1.5, 2, 3]) {
           const adjustedWidth = Math.round(width * density / 10) * 10;
@@ -164,79 +119,12 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
       }
 
       return result;
-    },
-    defaultSrcSet (): string[] {
-      return this.sources.slice(-1)[0].srcset;
-    },
-    defaultSrc (): string {
-      return this.defaultSrcSet[0];
-    },
-    styles (): Record<string, string> {
-      const styles = generatePlaceholderStyles(
-        this.src,
-        this.mobileSrc,
-        'image-block-height'
-      );
-
-      styles['--image-width'] = '100%';
-      if (this.width) {
-        styles['--image-width'] = this.width.toString();
-      }
-
-      return styles;
-    },
-    hasOverlay (): boolean {
-      return !!this.$slots.default;
     }
-  },
-  methods: {
-    getMediaQuery (breakpoint: number): string | undefined {
-      return `(max-width: ${breakpoint}px)`;
-    },
-    onLoad (): void{
-      this.isLoaded = true;
-    }
-  },
-  mounted () {
   }
 });
 </script>
 <style lang="scss" scoped>
-@import "~@storefront-ui/shared/styles/components/atoms/SfImage.scss";
-@import "~@storefront-ui/shared/styles/helpers/breakpoints";
-
 .storyblok-base-image {
   display: inline-block;
-  position: relative;
-  width: var(--image-width, 100%);
-
-  ._image-wrapper {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-
-  ._placeholder {
-    background-color: transparent;
-    padding-top: var(--image-block-height-mobile, var(--image-block-height, 0));
-  }
-
-  picture {
-   display: block;
-  }
-
-  &.-loading {
-    ._placeholder {
-      background-color: #fafafa;
-    }
-  }
-
-  @media (min-width: $tablet-min) {
-    ._placeholder {
-      padding-top: var(--image-block-height, 0);
-    }
-  }
 }
 </style>
