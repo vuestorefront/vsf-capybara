994e0e0a735394a63f05e6d5f57895b2fdc463e4
Kochnov Denis <d.kochnov.optimus@gmail.com>
2021-06-23 15:24:09 +0300

    Res #24684: Prepare pillow page component

--- pages/PillowProduct.vue
+++ pages/PillowProduct.vue
@@ -3,19 +3,10 @@
     <o-pillow-product-order-form
       :artwork-upload-url="artworkUploadUrl"
       :product="getCurrentProduct"
-      :product-id="getCurrentProduct.id + ''"
-      :product-sku="getCurrentProduct.sku"
-      :product-type="getBackendProductId"
-      :product-price="price.regular"
-      :product-special-price="price.special"
-      :product-quantity="getCurrentProduct.qty"
-      :product-description="getCurrentProduct.description"
-      :product-short-description="getCurrentProduct.short_description"
-      :product-name="getCurrentProduct.name"
-      :product-images="getProductImages"
-      :default-style="getDefaultStyle"
-      :available-styles="getAvailableStyles"
-      :addons="getAvailableAddons"
+      :backend-product-id="backendProductId"
+      :plushie-id="getPlushieId + ''"
+      :sizes="sizes"
+      :bodyparts="bodyparts"
     />
   </div>
 </template>
@@ -23,16 +14,15 @@
 <script lang="ts">
 import config from 'config';
 import { mapGetters } from 'vuex';
-import { getProductPrice } from 'theme/helpers';
 import { htmlDecode } from '@vue-storefront/core/filters';
 import { isServer } from '@vue-storefront/core/helpers';
 import { catalogHooksExecutors } from '@vue-storefront/core/modules/catalog-next/hooks';
-import { getProductGallery as getGalleryByProduct } from '@vue-storefront/core/modules/catalog/helpers';
 import { ProductValue } from 'src/modules/budsies';
 
-import OPillowProductOrderForm, { GalleryProductImages, SelectOption } from 'theme/components/organisms/o-pillow-product-order-form.vue';
-import { AddonOption } from 'theme/components/molecules/m-extra-faces.vue';
+import OPillowProductOrderForm from 'theme/components/organisms/o-pillow-product-order-form.vue';
+import BodypartOption from '../components/interfaces/bodypart-option';
 import { PropType } from 'vue';
+import Bodypart from 'src/modules/budsies/models/bodypart.model';
 
 export default {
   name: 'PillowProduct',
@@ -48,9 +38,6 @@ export default {
   computed: {
     ...mapGetters({
       getCurrentProduct: 'product/getCurrentProduct',
-      getProductGallery: 'product/getProductGallery',
-      getCurrentProductConfiguration: 'product/getCurrentProductConfiguration',
-      getOriginalProduct: 'product/getOriginalProduct',
       getCurrentCustomOptions: 'product/getCurrentCustomOptions',
       getCurrentPlushieId: 'budsies/getCurrentPlushieId'
     }),
@@ -61,125 +48,45 @@ export default {
 
       return this.getCurrentPlushieId;
     },
-    artworkUploadUrl () {
+    artworkUploadUrl (): string {
       return config.images.fileuploaderUploadUrl;
     },
-    price () {
-      return getProductPrice(this.getCurrentProduct, this.getCurrentCustomOptions, false);
-    },
-    getProductImages (): GalleryProductImages[] {
-      const images = this.getProductGallery.map((imageObject: any) => ({
-        stage: imageObject.src,
-        thumb: imageObject.src,
-        big: imageObject.src
-      }));
-
-      let result: GalleryProductImages[] = [
-        {
-          sku: this.getCurrentProduct.sku,
-          images: images
-        }
-      ]
-
+    sizes (): BodypartOption[] {
       if (!this.getCurrentProduct.bundle_options) {
-        return result;
+        throw new Error('The pillow product has no bundle options');
       }
 
+      let availableSizes: BodypartOption[] = [];
       for (const option of this.getCurrentProduct.bundle_options) {
         for (const productLink of option.product_links) {
-          const gallery = getGalleryByProduct(productLink.product);
-
-          const images = gallery.map((imageObject: any) => ({
-            stage: imageObject.src,
-            thumb: imageObject.src,
-            big: imageObject.src
-          }));
-
-          result.push({
-            sku: productLink.product.sku,
-            images: images
-          });
-        }
-      }
-
-      return result;
-    },
-    getDefaultStyle () {
-      if (this.getBackendProductId === ProductValue.PRINTED_KEYCHAINS) {
-        return 'simplePrintedKeychains';
-      }
-
-      return '';
-    },
-    getAvailableStyles () {
-      if (!this.getCurrentProduct.bundle_options) {
-        throw new Error('The printed product has no bundle options');
-      }
-
-      let availableStyles: SelectOption[] = [];
-      for (const option of this.getCurrentProduct.bundle_options) {
-        for (const productLink of option.product_links) {
-          if (!['simple', 'bundlePrimaryProduct'].includes(productLink.product.type_id)) {
+          if (!['bundlePrimaryProduct'].includes(productLink.product.type_id)) {
             continue;
           }
 
-          availableStyles.push({
-            optionId: option.option_id,
-            optionValueId: +productLink.id,
+          availableSizes.push({
+            id: productLink.product.id,
+            label: productLink.product.name + ' - $' + productLink.product.price,
             value: productLink.product.sku,
-            label: productLink.product.name,
-            description: productLink.product.description,
-            shortDescription: productLink.product.short_description,
-            price: productLink.product.regular_price,
-            specialPrice: productLink.product.special_price
+            isSelected: false,
+            image: productLink.product.image,
+            optionId: option.option_id,
+            optionValueId: productLink.id
           });
         }
       }
 
-      return availableStyles;
+      return availableSizes;
     },
-    getAvailableAddons (): AddonOption[] {
-      const addons = this.$store.getters['budsies/getPrintedProductAddons'](this.getCurrentProduct.id);
+    bodyparts (): Bodypart[] {
+      const bodyparts = this.$store.getters['budsies/getProductBodyparts'](this.getCurrentProduct.id);
 
-      if (!addons.length) {
+      if (!bodyparts.length) {
         return [];
       }
 
-      let addonOptions: Record<string, string|number>[] = [];
-
-      for (const option of this.getCurrentProduct.bundle_options) {
-        for (const productLink of option.product_links) {
-          if (productLink.product.type_id !== 'plushToyAddon') {
-            continue;
-          }
-
-          addonOptions.push({
-            optionId: option.option_id,
-            optionValueId: +productLink.id,
-            sku: productLink.product.sku
-          });
-        }
-      }
-
-      return addons.map(
-        (addon: any) => {
-          const addonOption = addonOptions.find(addonOption => addonOption.sku === addon.id);
-
-          if (!addonOption) {
-            throw new Error('The option product of printed product is not found');
-          }
-
-          return {
-            id: addon.id,
-            label: addon.label,
-            value: addon.value,
-            optionId: addonOption.optionId,
-            optionValueId: addonOption.optionValueId
-          }
-        }
-      );
+      return bodyparts;
     },
-    getBackendProductId (): ProductValue {
+    backendProductId (): ProductValue {
       switch (this.getCurrentProduct.id) {
         case 253:
           return ProductValue.PILLOW;
@@ -190,7 +97,7 @@ export default {
       }
     }
   },
-  async asyncData ({ store, route, context }) {
+  async asyncData ({ store, route, context }): Promise<void> {
     if (context) context.output.cacheTags.add('product')
 
     const product = await store.dispatch('product/loadProduct', {
@@ -201,6 +108,8 @@ export default {
     if (!this.plushieId && !store.getters['budsies/getCurrentPlushieId']) {
       await store.dispatch('budsies/createNewPlushie', { productId: product.id });
     }
+
+    await store.dispatch('budsies/loadProductBodyparts', { productId: product.id });
 
     const loadBreadcrumbsPromise = store.dispatch(
       'product/loadProductBreadcrumbs',
