624513b464240ffaf35fe21f18efae4634a35a53
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-24 18:05:57 +0300

    Refs #24341: Storyblok - Html - fix script tags processing

--- components/storyblok/Html.vue
+++ components/storyblok/Html.vue
@@ -4,15 +4,17 @@
     :class="cssClasses"
     :style="styles"
   >
-    <div
-      class="_html-wrapper"
-      v-html="itemData.html"
+    <div class="_no-script-html"
+         v-html="noScriptHtml"
     />
+
+    <div class="_script-container" ref="script-container" />
   </div>
 </template>
 
 <script lang="ts">
 import { Blok } from 'src/modules/vsf-storyblok-module/components'
+
 import HtmlData from './interfaces/html-data.interface';
 
 export default Blok.extend({
@@ -20,6 +22,46 @@ export default Blok.extend({
   computed: {
     itemData (): HtmlData {
       return this.item as HtmlData;
+    },
+    noScriptHtml (): string {
+      if (this.doesHtmlContainScript) {
+        return '';
+      }
+
+      return this.itemData.html;
+    },
+    doesHtmlContainScript (): boolean {
+      return this.itemData.html.includes('<script');
+    }
+  },
+  mounted (): void {
+    if (!this.doesHtmlContainScript) {
+      return;
+    }
+
+    this.setHtml(this.itemData.html);
+  },
+  methods: {
+    setHtml (html: string): void {
+      const container = this.$refs['script-container'] as HTMLElement | undefined;
+      if (!container) {
+        return;
+      }
+
+      container.innerHTML = html;
+      const scriptTags: HTMLScriptElement[] = Array.from(container.querySelectorAll('script'));
+
+      for (const oldScript of scriptTags) {
+        const newScript = document.createElement('script');
+        const scriptAttributes: Attr[] = Array.from(oldScript.attributes);
+
+        for (const attr of scriptAttributes) {
+          newScript.setAttribute(attr.name, attr.value);
+        }
+
+        newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+        oldScript.parentNode?.replaceChild(newScript, oldScript);
+      }
     }
   }
 })
