1e1b3aed302f3e893929bab2069cb470ee51a392
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-26 18:58:55 +0300

    Refs #24654: Storyblok - Heading - add "custom styled" mode

--- components/storyblok/interfaces/heading-data.interface.ts
+++ components/storyblok/interfaces/heading-data.interface.ts
@@ -2,5 +2,7 @@ import { ItemData } from 'src/modules/vsf-storyblok-module';
 
 export default interface HeadingData extends ItemData {
   title: string,
-  heading_type: string
+  heading_type: string,
+  is_custom_styled?: boolean,
+  intro_text: string
 }
