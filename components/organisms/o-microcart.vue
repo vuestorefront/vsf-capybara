4f7e0648cee1d122426b833833f1f7d87ee921bb
Azkya Khan <azkya.khan@hotwax.co>
2021-09-13 12:02:20 +0530

    Removed body scroll to fix scrolling issue (#v1.0.6)

--- components/organisms/o-microcart.vue
+++ components/organisms/o-microcart.vue
@@ -11,7 +11,6 @@
 import { mapState } from 'vuex';
 import ALoadingSpinner from 'theme/components/atoms/a-loading-spinner';
 import ALoadingError from 'theme/components/atoms/a-loading-error';
-// import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 
 const OMicrocartPanel = () =>
   import(/* webpackChunkName: "vsf-microcart" */ 'theme/components/organisms/o-microcart-panel');
@@ -36,17 +35,6 @@ export default {
       isMicrocartOpen: state => state.ui.microcart
     })
   },
-  // watch: {
-  //   isMicrocartOpen (state) {
-  //     if (state) {
-  //       this.$nextTick(() => {
-  //         disableBodyScroll(this.$el)
-  //       })
-  //     } else {
-  //       enableBodyScroll(this.$el)
-  //     }
-  //   }
-  // },
   methods: {
     reloadComponent () {
       this.theMicrocartAsyncComponent = () => ({
