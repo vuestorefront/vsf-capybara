994f3ce71a33b56193519764bffcc6c1c2951492
Andrey Vinogradov <andrei-vinogradov@yandex.ru>
2021-11-02 17:13:00 +0300

    Refs #25390: CartRecovery page - change forceClientState, remove debug code

--- pages/CartRecovery.vue
+++ pages/CartRecovery.vue
@@ -4,25 +4,16 @@
 
 <script lang="ts">
 import Vue from 'vue';
-// import config from 'config';
 import * as types from '../../../../core/modules/cart/store/mutation-types'
 import { localizedRoute } from '@vue-storefront/core/lib/multistore';
 
 export default Vue.extend({
   name: 'CartRecovery',
-  components: {
-
-  },
-  computed: {
-
-  },
   async mounted () {
     const { result, resultCode } = await this.$store.dispatch('budsies/loadRecoverableCart', {
       recoveryId: this.$route.params.id,
       recoveryCode: this.$route.params.code
     });
-
-    console.log(result);
 
     if (resultCode !== 200) {
       this.$router.push(localizedRoute('/error'))
@@ -30,7 +21,7 @@ export default Vue.extend({
 
     this.$store.commit('cart/' + types.CART_LOAD_CART_SERVER_TOKEN, result)
 
-    this.$store.dispatch('cart/load', { forceClientState: true })
+    this.$store.dispatch('cart/load', { forceClientState: false })
 
     this.$router.push(localizedRoute('/cart'))
   }
