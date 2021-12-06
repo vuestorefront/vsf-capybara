ef871f0b01b15b52ddd1d8798ce4b205884a0edc
Anton Sylka <anton.sylka@gorbunov.studio>
2021-12-06 16:56:50 +0800

    Refs #25524: Category page - fix CSR product fetch

--- pages/Category.vue
+++ pages/Category.vue
@@ -468,6 +468,7 @@ export default {
       next(async vm => {
         vm.loading = true;
         vm.currentPage = page;
+        await composeInitialPageState(store, to);
         await vm.$store.dispatch('category-next/cacheProducts', { route: to }); // await here is because we must wait for the hydration
         vm.loading = false;
       });
