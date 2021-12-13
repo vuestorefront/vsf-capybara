6469a6570c6457d435c4dd26947a82b42324e197
Anton Sylka <anton.sylka@gorbunov.studio>
2021-12-13 16:41:01 +0800

    Refs #25603: Implement component for floating photo displaying

---
+++ components/molecules/m-forevers-floating-photo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="o-forevers-creation-floating-photo" v-if="showFloatingPhoto">
+    <div class="_photo">
+      <img :src="image.src" alt="">
+    </div>
+
+    <SfHeading :title="petName" class="_name" :level="3" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+import { SfHeading } from '@storefront-ui/vue';
+
+import CustomerImage from '../interfaces/customer-image.interface';
+
+export default Vue.extend({
+  props: {
+    image: {
+      type: Object as PropType<CustomerImage | undefined>,
+      default: undefined
+    },
+    petName: {
+      type: String as PropType<string>,
+      default: ''
+    }
+  },
+  components: {
+    SfHeading
+  },
+  computed: {
+    showFloatingPhoto (): boolean {
+      return !!(this.image && this.image.url);
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.o-forevers-creation-floating-photo {
+  width: 100%;
+
+  ._name {
+      text-align: center;
+  }
+}
+</style>
