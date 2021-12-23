91c44c9883a0bc63752d03446e2ed9a26fdf6a5a
Andrey Vinogradov <andrei-vinogradov@yandex.ru>
2021-12-23 23:36:39 +0300

    Refs #25683: Petsies theme - add component for blocks rendering

---
+++ components/molecules/m-block-story.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="m-block-story">
+    <Blok v-if="showStory" :item="story.content" class="_story" />
+  </div>
+</template>
+
+<script lang="ts">
+import StoryMixin from 'theme/mixins/story';
+
+import { components } from 'src/modules/vsf-storyblok-module/components';
+
+const blocksFolderName = 'blocks';
+
+export default StoryMixin.extend({
+  name: 'MBlockStory',
+  props: {
+    storySlug: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    Blok: components.block
+  },
+  computed: {
+    storyFullSlug (): string {
+      // const normalizedPath = this.category.url_path.replace('/', '_');
+      return `${blocksFolderName}/${this.storySlug}`;
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
