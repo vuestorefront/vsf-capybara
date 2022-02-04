00e2e89b96cb9ef46d26d80e1ae44c5caf4eadee
Anton Sylka <anton.sylka@gorbunov.studio>
2022-02-04 16:58:21 +0800

    Refs #25925: Tweak price helper

--- helpers/price.ts
+++ helpers/price.ts
@@ -5,6 +5,7 @@ import { price } from '@vue-storefront/core/filters';
 import { getCustomOptionValues, getCustomOptionPriceDelta } from '@vue-storefront/core/modules/catalog/helpers/customOption'
 import { getBundleOptionsValues, getBundleOptionPrice } from '@vue-storefront/core/modules/catalog/helpers/bundleOptions'
 import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
+import { BundleOption } from '@vue-storefront/core/modules/catalog/types/BundleOption';
 
 import UpdateProductDiscountPriceEventData, { UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID } from 'src/modules/shared/types/update-product-discount-price.event';
 
@@ -14,7 +15,7 @@ interface ProductPriceData {
   specialPrice: number
 }
 
-function calculateBundleOptionsPrice (product) {
+function calculateCartItemBundleOptionsPrice (product) {
   const allBundleOptions = product.bundle_options || []
   const selectedBundleOptions = Object.values(get(product, 'product_option.extension_attributes.bundle_options', {}))
   const price = getBundleOptionPrice(
@@ -24,7 +25,17 @@ function calculateBundleOptionsPrice (product) {
   return price
 }
 
-function getProductPriceData (product): ProductPriceData {
+function calculateDefaultProductBundleOptionsPrice (product) {
+  const allBundleOptions = product.bundle_options || []
+  const requiredBundleOptions = product.bundle_options.filter((option: BundleOption) => option.required);
+  const price = getBundleOptionPrice(
+    getBundleOptionsValues(requiredBundleOptions as any[], allBundleOptions)
+  )
+
+  return price;
+}
+
+function getCartItemPriceData (product): ProductPriceData {
   let productPriceData: ProductPriceData = {
     originalPriceInclTax: 0,
     priceInclTax: 0,
@@ -32,7 +43,28 @@ function getProductPriceData (product): ProductPriceData {
   }
 
   if (isBundleProduct(product)) {
-    productPriceData = calculateBundleOptionsPrice(product);
+    productPriceData = calculateCartItemBundleOptionsPrice(product);
+  } else if (product.giftcard_options) {
+    productPriceData.priceInclTax = product.giftcard_options.price_amount;
+    productPriceData.originalPriceInclTax = product.giftcard_options.price_amount;
+  } else {
+    productPriceData.priceInclTax = product.price_incl_tax || product.priceInclTax || 0
+    productPriceData.originalPriceInclTax = product.original_price_incl_tax || product.originalPriceInclTax || 0
+    productPriceData.specialPrice = product.special_price || product.specialPrice || 0
+  }
+
+  return productPriceData;
+}
+
+function getDefaultProductPriceData (product) {
+  let productPriceData: ProductPriceData = {
+    originalPriceInclTax: 0,
+    priceInclTax: 0,
+    specialPrice: 0
+  }
+
+  if (isBundleProduct(product)) {
+    productPriceData = calculateDefaultProductBundleOptionsPrice(product);
   } else if (product.giftcard_options) {
     productPriceData.priceInclTax = product.giftcard_options.price_amount;
     productPriceData.originalPriceInclTax = product.giftcard_options.price_amount;
@@ -75,25 +107,10 @@ export function getProductDiscount (product, format = true) {
   return format ? `-${discount}%` : discount;
 }
 
-export function getProductPrice (product, customOptions = {}, format = true) {
-  if (!product) {
-    return {
-      regular: '',
-      special: ''
-    }
-  }
-
-  const productDiscountPriceData: UpdateProductDiscountPriceEventData = {
-    value: undefined,
-    product
-  }
-
-  EventBus.$emit(UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID, productDiscountPriceData);
-
+function getProductPrice (product, productDiscountPriceData: UpdateProductDiscountPriceEventData, productPriceData: ProductPriceData, customOptions = {}) {
   const productDiscountPrice = productDiscountPriceData.value
     ? productDiscountPriceData.value * (product.qty || 1)
     : productDiscountPriceData.value;
-  const productPriceData = getProductPriceData(product);
 
   let priceInclTax = productPriceData.priceInclTax;
   let originalPriceInclTax = productPriceData.originalPriceInclTax;
@@ -106,16 +123,65 @@ export function getProductPrice (product, customOptions = {}, format = true) {
   const original = (originalPriceInclTax + priceDelta) * product.qty || originalPriceInclTax
   const regular = (priceInclTax + priceDelta) * product.qty || product.regular_price || priceInclTax
 
-  if (!format) {
+  return {
+    regular: isSpecialPrice ? original : regular,
+    special: isSpecialPrice ? special : 0
+  }
+}
+
+export function getCartItemPrice (product, customOptions, format = true) {
+  if (!product) {
     return {
-      regular: isSpecialPrice ? original : regular,
-      special: isSpecialPrice ? special : 0
+      regular: '',
+      special: ''
     }
   }
 
+  const productPriceData = getCartItemPriceData(product);
+  const productDiscountPriceData: UpdateProductDiscountPriceEventData = {
+    value: undefined,
+    product
+  }
+
+  EventBus.$emit(UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID, productDiscountPriceData); // TODO replace with appropriate function
+
+  const productPrice = getProductPrice(product, productDiscountPriceData, productPriceData, customOptions);
+
+  if (!format) {
+    return productPrice;
+  }
+
   return {
-    regular: isSpecialPrice ? formatPrice(original) : formatPrice(regular),
-    special: isSpecialPrice ? formatPrice(special) : ''
+    regular: formatPrice(productPrice.regular),
+    special: formatPrice(productPrice.special)
+  }
+}
+
+export function getDefaultProductPrice (product, customOptions, format = true) {
+  if (!product) {
+    return {
+      regular: '',
+      special: ''
+    }
+  }
+
+  const productPriceData = getDefaultProductPriceData(product);
+  const productDiscountPriceData: UpdateProductDiscountPriceEventData = {
+    value: undefined,
+    product
+  }
+
+  EventBus.$emit(UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID, productDiscountPriceData); // TODO replace with appropriate function
+
+  const productPrice = getProductPrice(product, productDiscountPriceData, productPriceData, customOptions);
+
+  if (!format) {
+    return productPrice;
+  }
+
+  return {
+    regular: formatPrice(productPrice.regular),
+    special: formatPrice(productPrice.special)
   }
 }
 
