3abb80eae00a4d825a29a77a77018288f1058441
Anton Sylka <anton.sylka@gorbunov.studio>
2022-06-29 16:16:11 +0800

    Refs #26813: PillowProductOrderForm - use plushie-size-selector component

--- components/interfaces/size-option.ts
+++ components/interfaces/size-option.ts
@@ -2,5 +2,6 @@ import { BodypartOption } from 'src/modules/budsies';
 
 export default interface SizeOption extends BodypartOption {
   optionId: number,
-  optionValueId: string
+  optionValueId: string,
+  finalPrice: number
 }
--- components/organisms/o-plushie-size-selector.vue
+++ components/organisms/o-plushie-size-selector.vue
@@ -3,24 +3,25 @@
     <ul class="_selector">
       <li class="_selector-value"
           v-for="option in options"
-          :key="option.optionId"
+          :key="option.id"
       >
         <input
-          :id="inputIdDictionary[option.optionId]"
+          :id="inputIdDictionary[option.id]"
           type="radio"
           v-model="selectedOption"
+          :value="option"
           :disabled="disabled"
         >
-        <label :for="inputIdDictionary[option.optionId]">
-          <div class="_icon -most-popular" v-if="showMostPopularIconForOption(option)" />
-          <div class="_icon" :style="iconStyleDictionary[option.optionId]" />
+        <label :for="inputIdDictionary[option.id]">
+          <div class="_most-popular" v-if="showMostPopularIconForOption(option)" />
+          <div class="_icon" :style="iconStyleDictionary[option.id]" />
 
           <div class="_title">
-            {{ optionTitleDictionary[option.optionId] }}
+            {{ optionTitleDictionary[option.id] }}
           </div>
 
-          <div class="_price-delta" v-if="priceDeltaDictionary[option.optionId]">
-            {{ priceDeltaDictionary[option.optionId] }}
+          <div class="_price-delta" v-if="priceDeltaDictionary[option.id]">
+            {{ priceDeltaDictionary[option.id] }}
           </div>
 
         </label>
@@ -39,6 +40,7 @@ import SizeOption from '../interfaces/size-option'
 const SIZE_ITEM_WIDTH = 145;
 
 export default Vue.extend({
+  name: 'OPlushieSizeSelector',
   props: {
     value: {
       type: Object as PropType<SizeOption> | undefined,
@@ -73,8 +75,12 @@ export default Vue.extend({
       const iconStyleDictionary: Dictionary<Dictionary<string>> = {};
 
       this.options.forEach((option) => {
+        if (!option.image) {
+          return;
+        }
+
         const thumb = getThumbnailPath(option.image, SIZE_ITEM_WIDTH * 2, SIZE_ITEM_WIDTH * 2, 'product');
-        iconStyleDictionary[option.optionId] = {
+        iconStyleDictionary[option.id] = {
           'background-image': `url(${thumb})`
         }
       });
@@ -85,7 +91,7 @@ export default Vue.extend({
       const inputIdDictionary: Dictionary<string> = {};
 
       this.options.forEach((option) => {
-        inputIdDictionary[option.optionValueId] = `size_option_${option.optionId}`;
+        inputIdDictionary[option.id] = `size_option_${option.id}`;
       });
 
       return inputIdDictionary;
@@ -95,9 +101,9 @@ export default Vue.extend({
 
       this.options.forEach((option) => {
         if (this.isShowFullPrice(option)) {
-          optionTitleDictionary[option.optionId] = `${option.name} - $${option.finalPrice.toString()}`;
+          optionTitleDictionary[option.id] = `${option.label} - $${option.finalPrice.toString()}`;
         } else {
-          optionTitleDictionary[option.optionId] = option.name;
+          optionTitleDictionary[option.id] = option.label;
         }
       });
 
@@ -107,7 +113,7 @@ export default Vue.extend({
       const priceDeltaDictionary: Dictionary<string> = {};
 
       this.options.forEach((option) => {
-        if (this.showFullPrice(option)) {
+        if (this.isShowFullPrice(option) || !this.baseSizeOption) {
           return;
         }
 
@@ -130,7 +136,7 @@ export default Vue.extend({
   },
   methods: {
     isShowFullPrice (sizeOption: SizeOption): boolean {
-      return this.showFullPrices || this.baseSizeOption === sizeOption;
+      return this.showFullPrice || !this.baseSizeOption || this.baseSizeOption.id === sizeOption.id;
     },
     onSizeSelect (selectedSize: SizeOption): void {
       this.$emit('input', selectedSize);
@@ -143,7 +149,90 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.o-plushie-size-selector {
+$size-item-width: 145px;
 
+.o-plushie-size-selector {
+ ._selector {
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 0;
+    row-gap: 2vw;
+ }
+
+ ._selector-value {
+    box-sizing: border-box;
+    width: 33.33%;
+    padding: 0 2%;
+    max-width: $size-item-width;
+
+    ._icon {
+      background-position: center center;
+      background-repeat: no-repeat;
+      background-size: contain;
+      border-radius: 50%;
+      padding-top: 100%;
+      width: 100%;
+      position: relative;
+      z-index: 0;
+    }
+
+    ._most-popular {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    ._title {
+      font-size: var(--font-xs);
+      font-weight: var(--font-medium);
+      margin-top: var(--spacer-sm);
+    }
+
+    > input {
+      display: block;
+      height: 0;
+      opacity: 0;
+      width: 0;
+      margin: 0;
+      padding: 0;
+      border: 0;
+    }
+
+    > label {
+      height: auto;
+      margin: 0;
+      min-height: 0;
+      padding: 0;
+      width: 100%;
+      cursor: pointer;
+    }
+
+    > input:checked + label ._icon::before {
+    background: rgba(52, 184, 147, 0.7);
+    border-radius: 100%;
+    content: "";
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+  }
+
+  > input:checked + label ._icon::after {
+    background: url('/assets/images/sprite/ico-tick-green.png') no-repeat center #fff;
+    border: 2px solid #38b677;
+    border-radius: 100%;
+    content: "";
+    height: 24px;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 24px;
+    z-index: 2;
+  }
+ }
 }
 </style>
