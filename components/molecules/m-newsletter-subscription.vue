9abe6bbb8a97b6a6eceacdac50e6049be8d85006
Anton Sylka <anton.sylka@gorbunov.studio>
2022-07-12 13:45:50 +0800

    Refs #26888: Implement m-subscription-form component

--- components/molecules/m-newsletter-subscription.vue
+++ components/molecules/m-newsletter-subscription.vue
@@ -1,40 +1,26 @@
 <template>
   <div class="m-newsletter-subscription">
-    <slot />
-    <form @submit.prevent="subscribe" class="_subscription-form" v-show="displayForm">
-      <SfInput
-        v-model="email"
-        :name="emailInputName"
-        :label="$t('E-mail address')"
-        :disabled="isSubmitting"
-        :valid="!$v.email.$error"
-        :error-message="
-          !$v.email.required
-            ? $t('Field is required.')
-            : $t('Please provide valid e-mail address.')
-        "
-      />
-      <MSpinnerButton :show-spinner="isSubmitting">
-        Join
-      </MSpinnerButton>
-    </form>
-
-    <div class="_success-message" v-show="displaySuccessMessage">
-      {{ $t('Thank you for your subscription!') }}
-    </div>
+    <m-subscription-form
+      :name="name"
+      :is-submitting="isSubmitting"
+      :is-success="isSuccessSubscribed"
+      :error-message="errorMessage"
+      @email-changed="onEmailChanged"
+      @submit="subscribe"
+    >
+      <slot />
+    </m-subscription-form>
   </div>
 </template>
 
 <script lang="ts">
-import { getResponseMessage } from '@vue-storefront/core/lib/sync/helpers'
-import i18n from '@vue-storefront/i18n';
-import { SfInput } from '@storefront-ui/vue';
-import MSpinnerButton from 'theme/components/molecules/m-spinner-button.vue';
-import { required, email } from 'vuelidate/lib/validators';
+import MSubscriptionForm from './m-subscription-form.vue';
 
 export default {
   name: 'MNewsletterSubscription',
-  components: { SfInput, MSpinnerButton },
+  components: {
+    MSubscriptionForm
+  },
   props: {
     name: {
       type: String,
@@ -43,92 +29,40 @@ export default {
   },
   data (): Record<string, any> {
     return {
-      email: '',
       isSubmitting: false,
-      displayForm: true,
-      displaySuccessMessage: false
+      isSuccessSubscribed: false,
+      errorMessage: ''
     };
   },
-  computed: {
-    emailInputName (): string {
-      return this.name + '-email-input';
-    }
-  },
   methods: {
-    subscribe (): void {
-      this.$v.$touch();
-
-      if (this.$v.$invalid) {
-        return;
-      }
-
+    onEmailChanged (): void {
+      this.errorMessage = '';
+    },
+    subscribe (email: string): void {
       if (this.isSubmitting) {
         return;
       }
 
+      this.errorMessage = '';
       this.isSubmitting = true;
 
       this.$store.dispatch('budsies/createNewsletterSubscription', {
-        email: this.email
+        email
       }).then(res => {
+        if (res.result.errorMessage) {
+          this.errorMessage = res.result.errorMessage;
+          return;
+        }
+
         if (+res.resultCode !== 200) {
           return;
         }
 
-        this.displayForm = false;
-        this.displaySuccessMessage = true;
+        this.isSuccessSubscribed = true;
       }).finally(() => {
         this.isSubmitting = false;
       });
     }
-  },
-  validations: {
-    email: {
-      required,
-      email
-    }
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.m-newsletter-subscription {
-  --input-background: var(--_c-light-secondary);
-  ._subscription-form {
-    display: flex;
-    align-items: flex-start;
-  }
-  ::v-deep .sf-input {
-    &__label {
-      --input-label-font-size: var(--font-sm);
-      --input-label-left: var(--spacer-xs);
-    }
-    input {
-      --input-border: none;
-      --input-font-size: var(--font-sm);
-      --input-padding: var(--spacer-sm) 0 var(--spacer-xs) var(--spacer-xs);
-      &:focus {
-        & ~ * {
-          --input-label-font-size: var(--font-2xs);
-        }
-      }
-    }
-    &--has-text,
-    &--filled {
-      .sf-input__label {
-        --input-label-font-size: var(--font-2xs);
-      }
-    }
-  }
-  .m-spinner-button {
-    margin-left: var(--spacer-base);
-    --button-font-size: var(--font-xs);
-    --button-padding: calc(var(--spacer-base) * 0.56) var(--spacer-base);
-  }
-
-  ._success-message {
-    color: var(--c-text);
-    margin-bottom: calc(var(--font-xs) * 1.2);
-  }
-}
-</style>
